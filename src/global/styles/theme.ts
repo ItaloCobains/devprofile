c385ecf0aa015b0cd5c5d56eb8f8113252ab1996
ItaloCobains <monokatchaleger@gmail.com>
2022-10-05 15:14:41 -0300

    feat: theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -8,7 +8,7 @@ export default {
     success: '#12a454',
     danger: '#e83f5b',
     dark: '#121214',
-    ligth: '#f1f1f1',
+    light: '#f1f1f1',
     gray500: '#a8a8b3',
     gray800: '#29292e',
   },
