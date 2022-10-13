838ecfb09ee9338836a94081ab26a98e63d70021
ItaloCobains <monokatchaleger@gmail.com>
2022-10-13 15:39:45 -0300

    feat: AuthContext

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from '../services/api';
+import { Alert } from 'react-native';
+import { IUser } from '../model/user';
+
+interface IAuthState {
+  token: string;
+  user: IUser;
+}
 
 interface ICredentials {
   email: string;
@@ -7,7 +15,7 @@ interface ICredentials {
 }
 
 interface IAuthContext {
-  name: string;
+  user: IUser;
   signIn(credentials: ICredentials): void;
 }
 
@@ -19,21 +27,46 @@ export const AuthContext = React.createContext<IAuthContext>(
   {} as IAuthContext,
 );
 
+const tokenData = '@DevProfile:token';
+const userData = '@DevProfile:user';
+
 export const AuthProvider: React.FunctionComponent<Iprops> = ({ children }) => {
+  const [data, setData] = React.useState<IAuthState>({} as IAuthState);
+
+  React.useEffect(() => {
+    async function loadAuthData() {
+      const token = await AsyncStorage.getItem(tokenData);
+      const user = await AsyncStorage.getItem(userData);
+
+      if (token && user) {
+        setData({ token, user: JSON.parse(user) });
+      }
+    }
+    loadAuthData();
+  }, []);
+
   const signIn = async ({ email, password }: ICredentials) => {
     try {
       const response = await api.post('sessions', {
         email,
         password,
       });
-      console.log(response.data);
+      const { token, user } = response.data;
+
+      await AsyncStorage.setItem(tokenData, token);
+      await AsyncStorage.setItem(userData, JSON.stringify(user));
+      setData({ token, user });
     } catch (err) {
-      throw new Error(err as string);
+      // throw new Error(err as string);
+      Alert.alert(
+        'Erro na autenticação',
+        'Ocorreu um erro ao fazer o login, verifique as credenciais',
+      );
     }
   };
 
   return (
-    <AuthContext.Provider value={{ name: 'italo', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
